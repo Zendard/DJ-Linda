c2748a16535aa1b538c0fb6ab83609f8e3793a86
Zendard <thijsmuylaert@hotmail.com>
2024-10-02 19:33:35 +0200

    Initial boilerplate + ping command

---
+++ src/lib.rs
@@ -0,0 +1,15 @@
+pub struct Data {}
+type Error = Box<dyn std::error::Error + Send + Sync>;
+type Context<'a> = poise::Context<'a, Data, Error>;
+
+#[poise::command(slash_command, prefix_command)]
+pub async fn ping(ctx: Context<'_>) -> Result<(), Error> {
+    ctx.say("Pong!").await?;
+    Ok(())
+}
+
+#[poise::command(slash_command, prefix_command)]
+pub async fn register(ctx: Context<'_>) -> Result<(), Error> {
+    poise::builtins::register_application_commands_buttons(ctx).await?;
+    Ok(())
+}
--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,30 @@
-fn main() {}
+use poise::serenity_prelude as serenity;
+type Error = Box<dyn std::error::Error + Send + Sync>;
+
+#[tokio::main]
+async fn main() {
+    let framework = poise::Framework::builder()
+        .options(poise::FrameworkOptions {
+            commands: commands(),
+            ..Default::default()
+        })
+        .setup(|ctx, _ready, framework| {
+            Box::pin(async move {
+                poise::builtins::register_globally(ctx, &framework.options().commands).await?;
+                Ok(dj_linda::Data {})
+            })
+        })
+        .build();
+
+    let client = serenity::ClientBuilder::new(
+        env!("DISCORD_TOKEN"),
+        serenity::GatewayIntents::non_privileged(),
+    )
+    .framework(framework)
+    .await;
+    client.unwrap().start().await.unwrap();
+}
+
+fn commands() -> Vec<poise::Command<dj_linda::Data, Error>> {
+    vec![dj_linda::ping(), dj_linda::register()]
+}
